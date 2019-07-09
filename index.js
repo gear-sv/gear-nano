35277fb3e3d0416e9be03616bca6f0b8b35bf3b9
Sean Pollock <seanpollock3344@gmail.com>
2019-07-08 20:12:41 -0400

    adding leveldb instance and interlevel server

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const { planaria } = require("neonplanaria")
+const { promises: fs } = require("fs")
+const contractModule = require("./a.out.js")
+
+var kv
+
+const mockState = {
+  owner: "sean",
+  supply: 1000,
+  balances: {
+    sean: 500,
+    glenn: 300,
+    craig: 200
+  }
+}
 
 planaria.start({
   src: {
     from: 566470,
     path: `${process.cwd()}/../gear-bus/bus/87907d460d4823b94bf4458281e161d67a477d26fb1cb42825ee5cf8c0e4fe06/`
   },
-  onstart: function(e) {
-    console.log("inside onstart listener")
+  onstart: async function(e) {
+    // 1. instantiate contract
     
-    // instantiate wasm module
+    // 2. instantiate db instance
+    kv = level("kv", { valueEncoding: "json" })
+    L.server({ db: kv, port: 28335 })
+
   },
   onmempool: function(e) {
     console.log("inside onmempool listener")
@@ -16,6 +33,8 @@ planaria.start({
   onblock: function(e) {
     console.log("inside onblock listener")
 
-    // send transaction in relative order 
+    // 1. send transaction in relative order 
+    
+    // 2. save state snapshot
   },
 })
