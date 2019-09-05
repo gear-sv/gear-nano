f5e48b5b1d84d057b816da48c24f6d82fb77c3a6
gWOLF3 <wolfe.t.glenn@gmail.com>
2019-09-05 08:37:41 +0800

    fixed initializer issues

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,5 @@
 const { gearInit, createServer } = require("../gearia")
-const initializer = require("../initializer.js")
+const { initializeMachine, fetchBlueprint, fetchConstructor } = require("../initializer.js")
 
 
 const getters = {
@@ -9,10 +9,10 @@ const getters = {
 }
 
 // init machineConfig
-//initializeMachine("73cc7dd4937af750aa824f7b0f297e9fe7cca744379d08be74e738f7aa5d9afb")
+initializeMachine("73cc7dd4937af750aa824f7b0f297e9fe7cca744379d08be74e738f7aa5d9afb")
 
 //fetchBlueprint
-
+//fetchBlueprint()
 
 // 1. run block handler
 //gearia("63eec681025b07b9aa9d3720a125ce33dfd46e0b940a518100811c1f4eea86f0", getters)
--- initializer.js
+++ initializer.js
@@ -2,25 +2,34 @@ const { exec } = require("child_process")
 
 let transaction_id = "73cc7dd4937af750aa824f7b0f297e9fe7cca744379d08be74e738f7aa5d9afb"
 
-//const initializeMachine = (transaction_id) => {}
+const initializeMachine = (transaction_id) => {
+  fetchBlueprint(transaction_id)
+  fetchConstructor(transaction_id)
+}
 
-const fetchBlueprint = exec(`curl https://bico.media/${transaction_id} > gear-${transaction_id}.tar.gz && tar -xvzf gear-${transaction_id}.tar.gz`,
+const fetchBlueprint = (transaction_id) => {
+  exec(`curl https://bico.media/${transaction_id} > gear-${transaction_id}.tar.gz && tar -xvzf gear-${transaction_id}.tar.gz`,
 (error, stdout, stderr) => {
       if (error) console.log("#### problem fetching machine configuration from on chain", error)
       console.log(stdout)
       console.log(stderr)
-      return
     })
+}
 
 
-const fetchConstructor = exec(`curl https://api.whatsonchain.com/v1/bsv/main/tx/hash/73cc7dd4937af750aa824f7b0f297e9fe7cca744379d08be74e738f7aa5d9afb | jq '.vout[1].scriptPubKey.addresses[0]'`, (error, stdout, stderr) => {
+const fetchConstructor = (transaction_id) => {
+exec(`curl https://api.whatsonchain.com/v1/bsv/main/tx/hash/${transaction_id} | jq '.vout[1].scriptPubKey.addresses[0]'`, (error, stdout, stderr) => {
       if (error) console.log("#### problem fetching machine configuration from on chain", error)
       console.log(stdout)
       console.log(stderr)
-      const owner = this.stdout
-      console.log(this.owner)
+      // const owner = this.stdout
+      // console.log(this.owner)
     })
+}
 
-// const test = exec(`echo HIT!!`, (error, stdout, stderr) => {
-//   console.log(stdout)
-// })
+
+module.exports = {
+  initializeMachine,
+  fetchBlueprint,
+  fetchConstructor
+}
